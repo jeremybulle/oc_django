7ded8033b4b53c2c96c5a94d5467ff99a58a53bf
jeremybulle <jeremyjp.bulle@gmail.com>
2019-11-25 13:56:05 +0100

    ajouter de la 1ere vue

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .models import ALBUMS
+
+from django.http import HttpResponse
+
+
+
 
 # Create your views here.
+
+def index(request):
+    message = "Salut tout le monde"
+    return HttpResponse(message)
+
+def listing(request):
+    albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
+
+    message = """<ul>{}</ul>""".format("\n".join(albums))
+    return HttpResponse(message)
